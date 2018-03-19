ea4343af87aaf41d840ce4a036f00cc614991a19
Seth Bromberger <github@bromberger.com>
2018-03-18 18:58:13 -0700

    efficiency

--- rat.hpp
+++ rat.hpp
@@ -1,42 +1,32 @@
 #include <iostream>
 #include <string>
+#include <numeric>
 
 class Rational {
-    const int gcd, num, den;
+    int gcd, num, den;
 
-    int calc_gcd(int, int);
     friend std::ostream &operator<<(std::ostream &os, Rational const &r);
 
   public:
     Rational(int n, int d, int g) : gcd{g}, num{n}, den{d}{};
     Rational(int, int);
     Rational(int n): Rational(1, n, 1) {};
-    std::string string();
-    Rational operator+(Rational);
-    Rational operator-() { return {-num, den}; };
-    Rational operator-(Rational);
-    Rational operator*(Rational);
-    Rational operator/(Rational);
+    std::string string() const;
+    Rational operator+(Rational) const;
+    Rational operator-() const { return {-num, den}; };
+    Rational operator-(Rational) const;
+    Rational operator*(Rational) const;
+    Rational operator/(Rational) const;
 };
 
-int Rational::calc_gcd(int n, int d) {
-    int a = n, b = d;
-    while (b != 0) {
-        auto m = a % b;
-        a = b;
-        b = m;
-    }
-    return a;
-}
-
-Rational::Rational(int n, int d):gcd{Rational::calc_gcd(n, d)}, num{n / gcd}, den{d / gcd}{
+Rational::Rational(int n, int d):gcd{std::gcd(n, d)}, num{n / gcd}, den{d / gcd}{
     if (d == 0) {
         std::cerr << "Division by zero error." << std::endl;
         abort();
     }
 }
 
-std::string Rational::string() {
+std::string Rational::string() const {
     return "num = " + std::to_string(num) + ", den = " + std::to_string(den) + ", gcd = " + std::to_string(gcd);
 }
 
@@ -44,21 +34,21 @@ std::ostream &operator<<(std::ostream &os, Rational const &r) {
     return os << r.num << "//" << r.den;
 }
 
-Rational Rational::operator+(Rational r) {
+Rational Rational::operator+(Rational r) const {
     auto m = den * r.den;
     auto n1 = num * r.den;
     auto n2 = r.num * den;
     return Rational(n1 + n2, m);
 }
 
-Rational Rational::operator-(Rational r) {
+Rational Rational::operator-(Rational r) const {
     return *this + -r;
 }
 
-Rational Rational::operator*(Rational r) {
+Rational Rational::operator*(Rational r) const {
     return Rational(num * r.num, den * r.den);
 }
 
-Rational Rational::operator/(Rational r) {
+Rational Rational::operator/(Rational r) const {
     return Rational(num * r.den, den * r.num);
 }
