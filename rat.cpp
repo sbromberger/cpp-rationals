95fba720201f2e27df1c39abdf3ccdde780f6d34
Seth Bromberger <github@bromberger.com>
2018-03-19 10:00:31 -0700

    format

--- rat.cpp
+++ rat.cpp
@@ -1,24 +1,25 @@
 #include "rat.hpp"
 
 int main() {
-    auto r1 = Rational(2, 3);
-    auto r2 = Rational(10, 20);
-    auto r3 = r1 + r2;
-    auto r4 = -r1;
-    auto r5 = r1 - r2;
-    auto r6 = Rational(2);
-    std::cout << r1 << ": " << r1.string() << std::endl;
-    std::cout << r2 << ": " << r2.string() << std::endl;
-    std::cout << r3 << ": " << r3.string() << std::endl;
-    std::cout << r4 << ": " << r4.string() << std::endl;
-    std::cout << r5 << ": " << r5.string() << std::endl;
-    std::cout << (r6 * r2) << ": " << (r6 * r2).string() << std::endl;
-    std::cout << (r6 / r2) << ": " << (r6 / r2).string() << std::endl;
-    std::cout << "Rational equals" << std::endl;
-    std::cout << (r1 == Rational(8, 12)) << std::endl;
-    std::cout << (Rational(2, 3).string()) << " and " << (Rational(4, 6).string()) << std::endl;
-    std::cout << "Rational lt" << std::endl;
-    std::cout << (Rational(2, 3) < Rational(6, 7)) << std::endl;
-    std::cout << (Rational(2, 3) < Rational(1, 7)) << std::endl;
-    return (0);
+  auto r1 = Rational(2, 3);
+  auto r2 = Rational(10, 20);
+  auto r3 = r1 + r2;
+  auto r4 = -r1;
+  auto r5 = r1 - r2;
+  auto r6 = Rational(2);
+  std::cout << r1 << ": " << r1.string() << std::endl;
+  std::cout << r2 << ": " << r2.string() << std::endl;
+  std::cout << r3 << ": " << r3.string() << std::endl;
+  std::cout << r4 << ": " << r4.string() << std::endl;
+  std::cout << r5 << ": " << r5.string() << std::endl;
+  std::cout << (r6 * r2) << ": " << (r6 * r2).string() << std::endl;
+  std::cout << (r6 / r2) << ": " << (r6 / r2).string() << std::endl;
+  std::cout << "Rational equals" << std::endl;
+  std::cout << (r1 == Rational(8, 12)) << std::endl;
+  std::cout << (Rational(2, 3).string()) << " and " << (Rational(4, 6).string())
+            << std::endl;
+  std::cout << "Rational lt" << std::endl;
+  std::cout << (Rational(2, 3) < Rational(6, 7)) << std::endl;
+  std::cout << (Rational(2, 3) < Rational(1, 7)) << std::endl;
+  return (0);
 }
--- rat.hpp
+++ rat.hpp
@@ -1,74 +1,73 @@
 #include <iostream>
-#include <string>
 #include <numeric>
+#include <string>
 
 class Rational {
-    int gcd, num, den;
+  int gcd, num, den;
 
-    friend std::ostream &operator<<(std::ostream &os, Rational const &r);
+  friend std::ostream &operator<<(std::ostream &os, Rational const &r);
 
-  public:
-    Rational(int n, int d, int g) : gcd{g}, num{n}, den{d}{};
-    Rational(int, int);
-    Rational(int n): Rational(1, n, 1) {};
-    std::string string() const;
-    Rational operator+(Rational) const;
-    Rational operator-() const { return {-num, den}; };
-    Rational operator-(Rational) const;
-    Rational operator*(Rational) const;
-    Rational operator/(Rational) const;
-    bool operator==(Rational) const;
-    bool operator<(Rational) const;
-    bool operator>(Rational) const;
-    bool operator!=(Rational r) const { return !(*this == r); };
-    bool operator<=(Rational r) const { return !(*this > r); };
-    bool operator>=(Rational r) const { return !(*this < r); };
+ public:
+  Rational(int n, int d, int g) : gcd{g}, num{n}, den{d} {};
+  Rational(int, int);
+  Rational(int n) : Rational(1, n, 1){};
+  std::string string() const;
+  Rational operator+(Rational) const;
+  Rational operator-() const { return {-num, den}; };
+  Rational operator-(Rational) const;
+  Rational operator*(Rational) const;
+  Rational operator/(Rational) const;
+  bool operator==(Rational) const;
+  bool operator<(Rational) const;
+  bool operator>(Rational) const;
+  bool operator!=(Rational r) const { return !(*this == r); };
+  bool operator<=(Rational r) const { return !(*this > r); };
+  bool operator>=(Rational r) const { return !(*this < r); };
 };
 
-inline Rational::Rational(int n, int d):gcd{std::gcd(n, d)}, num{n / gcd}, den{d / gcd}{
-    if (d == 0) {
-        std::cerr << "Division by zero error." << std::endl;
-        throw std::runtime_error("Division by zero error!");
-    }
+inline Rational::Rational(int n, int d) {
+  if (d == 0) throw std::runtime_error("division by zero");
+  gcd = std::gcd(n, d);
+  num = n / gcd;
+  den = d / gcd;
 }
 
 inline std::string Rational::string() const {
-    return "num = " + std::to_string(num) + ", den = " + std::to_string(den) + ", gcd = " + std::to_string(gcd);
+  return "num = " + std::to_string(num) + ", den = " + std::to_string(den) +
+         ", gcd = " + std::to_string(gcd);
 }
 
 inline std::ostream &operator<<(std::ostream &os, Rational const &r) {
-    return os << r.num << "//" << r.den;
+  return os << r.num << "//" << r.den;
 }
 
 inline Rational Rational::operator+(Rational r) const {
-    auto m = den * r.den;
-    auto n1 = num * r.den;
-    auto n2 = r.num * den;
-    return Rational(n1 + n2, m);
+  auto m = den * r.den;
+  auto n1 = num * r.den;
+  auto n2 = r.num * den;
+  return Rational(n1 + n2, m);
 }
 
-inline Rational Rational::operator-(Rational r) const {
-    return *this + -r;
-}
+inline Rational Rational::operator-(Rational r) const { return *this + -r; }
 
 inline Rational Rational::operator*(Rational r) const {
-    return Rational(num * r.num, den * r.den);
+  return Rational(num * r.num, den * r.den);
 }
 
 inline Rational Rational::operator/(Rational r) const {
-    return Rational(num * r.den, den * r.num);
+  return Rational(num * r.den, den * r.num);
 }
 
 inline bool Rational::operator==(Rational r) const {
-    return (num == r.num) && (den == r.den);
+  return (num == r.num) && (den == r.den);
 }
 
 inline bool Rational::operator<(Rational r) const {
-    auto n = *this - r;
-    return n.num < 0;
+  auto n = *this - r;
+  return n.num < 0;
 }
 
 inline bool Rational::operator>(Rational r) const {
-    auto n = *this - r;
-    return n.num > 0;
+  auto n = *this - r;
+  return n.num > 0;
 }
